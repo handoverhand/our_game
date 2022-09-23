653519acd479fa5420dc0f2df1452fb5bacdc80e
Alina_Meishele <alina.meishele@gmail.com>
2022-09-23 12:20:03 +0300

    dynamic count done

--- frontend/src/features/Game/Game.jsx
+++ frontend/src/features/Game/Game.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import QuestionCard from './QuestionCard';
+import UserContext from '../Context/Context';
 
 function Game() {
   const [questions, setQuestions] = useState([]);
   const [topics, setTopics] = useState([]);
-  const [actualscore, setActualscore] = useState(null);
+  // const [actualscore, setActualscore] = useState(null);
+  const [context, setContext] = useContext(UserContext);
 
   useEffect(() => {
     fetch('/data/questions')
@@ -23,14 +25,12 @@ function Game() {
   useEffect(() => {
     fetch('/data/actualscore')
       .then((data) => data.json())
-      .then((res) => setActualscore(res));
+      .then((res) => setContext({ ...context, score: res }));
   });
 
   return (
     <>
-      <div>
-        Общее количество очков в раунде: {actualscore > 0 ? actualscore : 0}
-      </div>
+      <div>Общее количество очков в раунде: {context.score}</div>
       <div className="game-container">
         <div className="topic-container">
           {topics.map((item) => (
@@ -42,7 +42,7 @@ function Game() {
         <div className="question-container">
           {questions.map((item) => (
             // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-            <QuestionCard key={item.id} item={item} actualscore={actualscore} />
+            <QuestionCard key={item.id} item={item} />
           ))}
         </div>
       </div>
